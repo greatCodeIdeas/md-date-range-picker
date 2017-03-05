6672232a0d4d2194bca7b4dbba70e9eec221acc3
YoniA <yehonatan.argovi@hp.com>
2017-03-05 15:53:12 +0200

    updated the readme with the new localization support

--- src/md-date-range-picker.js
+++ src/md-date-range-picker.js
@@ -170,7 +170,6 @@
         $scope.handleClickSelectLastYear = handleClickSelectLastYear;
 
         $scope.getLocalizationVal = getLocalizationVal;
-        //$scope.localizationMap = localizationMap;
        
         init();
        
